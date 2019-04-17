55bfe09d194744d454b6a709fc7ca2f5f9e82cb2
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-17 15:19:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
-  array.each { |index| index = index ** 2
-    puts index
-    puts array[]
+  index = 0
+  array.each { |indexNum|
+  array[index] = indexNum ** 2
+  index += 1
   }
 end
