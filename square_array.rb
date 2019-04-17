8d99ac22eff9a5372b048404d318cc2799c273b6
Solomon Feldman <solomonrfeldman@gmail.com>
2019-04-17 15:42:01 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
-  array.collect { |num|
-    num **= 2
-  }
-  # index = 0
-  # array.each { |num|
-  #   array[index] = num ** 2
-  #   index += 1
+  # array.collect { |num|
+  #   num **= 2
   # }
+  index = 0
+  array.each { |num|
+    array[index] = num ** 2
+    index += 1
+  }
 end
